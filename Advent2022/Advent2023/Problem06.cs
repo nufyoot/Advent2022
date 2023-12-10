b4b8c41230e3c15748c574295bd21e434ce08858
Terry Tice <ttice@prsmhealthcare.com>
2023-12-09 20:54:30 -0500

    Checkpoint - working on problem 6

---
+++ Advent2022/Advent2023/Problem06.cs
@@ -0,0 +1,23 @@
+﻿namespace Advent2022.Advent2023;
+
+public static class Problem06
+{
+    public static async Task<string> SolvePart1Async(Stream input)
+    {
+        var (times, distances) = await ParseRaces(input);
+        throw new NotImplementedException();
+    }
+    
+    public static async Task<string> SolvePart2Async(Stream input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task<(List<int> times, List<int> distances)> ParseRaces(Stream input)
+    {
+        using StreamReader reader = new(input);
+        var times = (await reader.ReadLineAsync())![9..].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        var distances = (await reader.ReadLineAsync())![9..].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        return (times, distances);
+    }
+}
